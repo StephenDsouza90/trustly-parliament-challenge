7b0d98626f2bf00d46f7c6074d10cbd4b0f62aea
StephenDsouza90 <dsouza_stephen@hotmail.com>
2019-11-30 21:18:58 +0100

    Re-wrote code

--- parliament_challenge.py
+++ parliament_challenge.py
@@ -9,150 +9,158 @@ success = {"message": "Success"}
 failed = {"message": "Failed"}
 
 
+def get_speeches():
+    # use speeches api to extract data
+    domain = 'http://data.riksdagen.se'
+    speeches = 'anforandelista'
+    size = 10
+    format_type = 'json'
+    response = requests.get('{}/{}/?anftyp=Nej&sz={}&utformat={}'.format(domain, speeches, size, format_type))
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    elif response.status_code == 404:
+        return json.dumps(not_found)
+
+
+def get_members():
+    # use members api to extract data
+    domain = 'http://data.riksdagen.se'
+    members = 'personlista'
+    format_type = 'json'
+    response = requests.get('{}/{}/?iid=&fnamn=&enamn=&f_ar=&kn=&parti=&valkrets=&rdlstatus=&org=&utformat={}&sort=sorteringsnamn&sortorder=asc&termlista='.format(domain, members, format_type))
+
+    if response.status_code == 200:
+        data = response.json()
+        return data
+    elif response.status_code == 404:
+        return json.dumps(not_found)
+
+
+def recursive_items(dictionary):
+    # loop through nested dict
+    # skips 1st nested loop
+    for key, value in dictionary.items():
+        if type(value) is dict:
+            yield from recursive_items(value)
+        else:
+            yield (key, value)
+
+
+def get_speeches_dict():
+    # get speeches data from get_speeches func
+    speeches_data = get_speeches()
+
+    # loop through speeches_data to return relevant keys and values of ten latest speeches
+    relevant_speeches = []
+    for keys, values in recursive_items(speeches_data):
+        for keys in values:
+            anforande_id = [keys[x] for x in keys if x == "anforande_id"]
+            dok_datum = [keys[x] for x in keys if x == "dok_datum"]
+            talare = [keys[x] for x in keys if x == "talare"]
+            parti = [keys[x] for x in keys if x == "parti"]
+            protokoll_url_www = [keys[x] for x in keys if x == "protokoll_url_www"]
+            dok_titel = [keys[x] for x in keys if x == "dok_titel"]
+            intressent_id = [keys[x] for x in keys if x == "intressent_id"] # to be removed later
+
+            speechesDict = {
+                "anforande_id": anforande_id,
+                "dok_datum": dok_datum,
+                "talare": talare,
+                "parti": parti, 
+                "protokoll_url_www": protokoll_url_www, 
+                "dok_titel": dok_titel,
+                "intressent_id": intressent_id
+                }
+            relevant_speeches.append(speechesDict)
+    return relevant_speeches
+
+
+def get_members_dict():
+    # get members data from get_members func
+    members_data = get_members()
+
+    # loop through members_data to return relevant values of ONLY the ten latest speeches
+    NOT_relevant_members = []
+    for keys, values in recursive_items(members_data):
+        for keys in values:
+            valkrets = [keys[x] for x in keys if x == "valkrets"]
+            bild_url_192 = [keys[x] for x in keys if x == "bild_url_192"]
+            uppgift = [keys[x] for x in keys if x == "uppgift"] # email not come
+            intressent_id = [keys[x] for x in keys if x == "intressent_id"] # to be removed later
+
+            membersDict = {
+                "valkrets": valkrets,
+                "bild_url_192": bild_url_192,
+                "uppgift": uppgift,
+                "intressent_id_check": intressent_id
+                }
+            NOT_relevant_members.append(membersDict)
+    return NOT_relevant_members
+
+
+def get_relevant_members_dict():
+    speeches_data = get_speeches_dict()
+    members_data = get_members_dict()
+
+    # access the value of FK in speeches_data and match that with the value of FK in the members_data
+    relevant_members_dict = []
+    for x in speeches_data:
+        value_intressent_id = x["intressent_id"]
+        for y in members_data:
+            value_intressent_id_check = y["intressent_id_check"]
+            if value_intressent_id == value_intressent_id_check:
+                rel = y
+                relevant_members_dict.append(rel)
+    return relevant_members_dict
+
+
 def create_app():
     app = Flask("Parliament Challenge")
-
-    def get_speeches():
-        # use speeches api to extract data
-        domain = 'http://data.riksdagen.se'
-        speeches = 'anforandelista'
-        size = 10
-        format_type = 'json'
-        response = requests.get('{}/{}/?anftyp=Nej&sz={}&utformat={}'.format(domain, speeches, size, format_type))
-
-        if response.status_code == 200:
-            data = response.json()
-            return data
-        elif response.status_code == 404:
-            print(not_found)
-            return json.dumps(not_found)
-
-    def get_members():
-        # use members api to extract data
-        domain = 'http://data.riksdagen.se'
-        members = 'personlista'
-        format_type = 'json'
-        response = requests.get('{}/{}/?iid=&fnamn=&enamn=&f_ar=&kn=&parti=&valkrets=&rdlstatus=&org=&utformat={}&sort=sorteringsnamn&sortorder=asc&termlista='.format(domain, members, format_type))
-
-        if response.status_code == 200:
-            data = response.json()
-            return data
-        elif response.status_code == 404:
-            print(not_found)
-            return json.dumps(not_found)
-
-    def recursive_items(dictionary):
-        # loop through nested dict
-        # skips 1st nested loop
-        for key, value in dictionary.items():
-            if type(value) is dict:
-                yield from recursive_items(value)
-            else:
-                yield (key, value)
-
-    def get_foreignkey_speeches():
-        # Get speeches data from get_speeches func
-        speeches_data = get_speeches()
-
-        # Loop through speeches_data to find "intressent_id" which is the foreign key
-        speech_foreignkey_list = []
-        for keys, values in recursive_items(speeches_data):
-            for keys in values:
-                for x in keys:
-                    if x == "intressent_id":
-                        speech_foreignkey_list.append(keys[x])
-        return speech_foreignkey_list
-
-    def get_foreignkey_members():
-        # Get members data from get_members func
-        members_data = get_members()
-
-        # Loop through members_data to find "intressent_id" which is the foreign key
-        members_foreignkey_list = []
-        for keys, values in recursive_items(members_data): 
-            for keys in values:
-                for x in keys:
-                    if x == "intressent_id":
-                        members_foreignkey_list.append(keys[x])
-        return members_foreignkey_list
-
-    def speeches_dict():
-        # Get speeches data from get_speeches func
-        speeches_data = get_speeches()
-
-        # loop through speeches_data to return relevant values of ten latest speeches        
-        for keys, values in recursive_items(speeches_data):
-            for keys in values:
-                anforande_id = [keys[x] for x in keys if x == "anforande_id"]
-                dok_datum = [keys[x] for x in keys if x == "dok_datum"]
-                talare = [keys[x] for x in keys if x == "talare"]
-                parti = [keys[x] for x in keys if x == "parti"]
-                protokoll_url_www = [keys[x] for x in keys if x == "protokoll_url_www"]
-                dok_titel = [keys[x] for x in keys if x == "dok_titel"]
-                intressent_id = [keys[x] for x in keys if x == "intressent_id"] # to be removed later
-        return anforande_id, dok_datum, talare, parti, protokoll_url_www, dok_titel, intressent_id
-    # printing only 1 .. need to find a way to print all ten
-
-    def members_dict():
-        # Get members data from get_members func
-        members_data = get_members()
-
-        # Get foreign keys list from get_foreignkey_speeches and get_foreignkey_members func
-        foreignkey_speeches = get_foreignkey_speeches()
-        foreignkey_members = get_foreignkey_members()
-
-        # loop through foreignkey_speeches and find a match in foreignkey_members
-        for i in foreignkey_speeches:
-            for j in foreignkey_members:
-                if i == j:
-                    # loop through members_data to return relevant values of ONLY the ten latest speeches
-                    for keys, values in recursive_items(members_data):
-                        for keys in values:
-                            valkrets = [keys[x] for x in keys if x == "valkrets"]
-                            bild_url_192 = [keys[x] for x in keys if x == "bild_url_192"]
-                            uppgift = [keys[x] for x in keys if x == "uppgift"] # email not come
-                            intressent_id = [keys[x] for x in keys if x == "intressent_id"] 
-        return valkrets, bild_url_192, uppgift, intressent_id
-    # Problem with matching foreign keys because someone else details are coming
-    # printing only 1 .. need to find a way to print all ten 
-
+  
     @app.route('/ten-latest-speeches', methods=['GET'])
     def get_ten_latest_speeches():
         """
         curl -X GET "localhost:8080/ten-latest-speeches"
         """
+        speeches = get_speeches_dict()
+        members = get_relevant_members_dict()
 
-        # Get relevant speeches data list from speeches_dict func
-        speeches_data = speeches_dict()
-        # Get relevant memebers data list from members_dict func
-        members_data = members_dict()
-
-        # Empty list to merge both data together
+        # merge both data together
         ten_latest_speeches = []
+        for s in speeches:
+            for m in members:
+                anforande_id = s["anforande_id"]
+                dok_datum = s["dok_datum"]
+                talare = s["talare"]
+                parti = s["parti"]
+                protokoll_url_www = s["protokoll_url_www"]
+                dok_titel = s["dok_titel"]
+                intressent_id = s["intressent_id"]
+                valkrets = m["valkrets"]
+                bild_url_192 = m["bild_url_192"]
+                uppgift = m["uppgift"]
+                intressent_id_check = m["intressent_id_check"]
 
-        # Since speeches_data and members_data and tuples so use []
-        if speeches_data:
-            if members_data:
                 combinedDict = {
-                    "anforande_id": speeches_data[0],
-                    "dok_datum": speeches_data[1],
-                    "talare": speeches_data[2],
-                    "parti": speeches_data[3], 
-                    "protokoll_url_www": speeches_data[4], 
-                    "dok_titel": speeches_data[5],
-                    "intressent_id": speeches_data[6],
-                    "valkrets": members_data[0],
-                    "bild_url_192": members_data[1],
-                    "uppgift": members_data[2],
-                    "intressent_id_check": members_data[3]
+                    "anforande_id": anforande_id,
+                    "dok_datum": dok_datum,
+                    "talare": talare,
+                    "parti": parti, 
+                    "protokoll_url_www": protokoll_url_www, 
+                    "dok_titel": dok_titel,
+                    "intressent_id": intressent_id,
+                    "valkrets": valkrets,
+                    "bild_url_192": bild_url_192,
+                    "uppgift": uppgift,
+                    "intressent_id_check": intressent_id_check
                     }
-
                 ten_latest_speeches.append(combinedDict)
-
-        print(ten_latest_speeches)
-        return json.dumps(ten_latest_speeches)
+        # print(ten_latest_speeches)
+        # return json.dumps(ten_latest_speeches)
     return app
-
+    
 
 def main():
     app = create_app()
