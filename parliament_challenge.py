fdc8c0a8e14a51da7f2d42666cb5fc97102dc276
StephenDsouza90 <dsouza_stephen@hotmail.com>
2020-05-22 00:07:33 +0200

    Refactored code structure

--- parliament_challenge.py
+++ parliament_challenge.py
@@ -1,7 +1,67 @@
 import requests
 import waitress
 import flask
+
 from flask import Flask, request, json
+
+
+def create_app():
+    app = Flask("Parliament Challenge")
+
+    @app.route('/latest-speeches/', methods=['GET'])
+    def get_latest_speeches():
+        """
+        GET request:
+            curl -X GET "localhost:8080/latest-speeches/?anftyp=Nej&sz=10"
+        """
+        """
+        Clients can input the number of speeches required by them.
+        Result will be a merged data of speeches with members. 
+        """
+
+        anftyp = flask.request.args["anftyp"]
+        size = flask.request.args["sz"]
+        speeches = get_speeches_data(anftyp, size)
+        return json.dumps(speeches)
+    return app
+
+
+def get_speeches_data(anftyp, size):
+    """
+    Get speeches data from speech's api
+    and filter for relevant keys.
+    For each speech we query the member api 
+    to get the member data and 
+    then merge it with the speech data.
+    """
+
+    domain = 'http://data.riksdagen.se'
+    speeches = 'anforandelista'
+    format_type = 'json'
+    response = requests.get('{}/{}/?anftyp={}&sz={}&utformat={}'.format(domain, speeches, anftyp, size, format_type))
+    if response.status_code == 200:
+        data = response.json()
+        # When a server responds with more than one speech.
+        if data["anforandelista"]["@antal"] > "1":
+            speeches = data["anforandelista"]["anforande"]
+            filtered_speeches = filter_speeches_dict(speeches)
+            for speech in filtered_speeches:
+                member_data = get_member_data(speech["intressent_id"])        
+                speech.update(member_data)
+            return filtered_speeches
+        # When a server responds only with one speech.
+        # A dict is returned and to handle this, the dict is stored 
+        # in a list because the filter_speeches_dict accepts a list.
+        elif data["anforandelista"]["@antal"] == "1":
+            speech = [
+                data["anforandelista"]["anforande"]
+            ]
+            filtered_speech = filter_speeches_dict(speech)[0]
+            member_data = get_member_data(filtered_speech["intressent_id"])
+            filtered_speech.update(member_data)
+            return filtered_speeches
+    else:
+        return []
 
 
 def filter_speeches_dict(speeches):
@@ -21,11 +81,37 @@ def filter_speeches_dict(speeches):
                 "links": [{ 
                     "rel": "speech",
                     "href": s.get("protokoll_url_www")
-                        }],
+                    }],
                 "intressent_id": s.get("intressent_id")
                 }
         speeches_list.append(speechesDict)
     return speeches_list
+
+
+def get_member_data(intressent_id):
+    """
+    Get a member data from the member's api
+    and filter for relevant keys.
+    """
+
+    domain = 'http://data.riksdagen.se'
+    members = 'personlista'
+    format_type = 'json'
+    response = requests.get('{}/{}/?iid={}&utformat={}'.format(domain, members, intressent_id, format_type))
+    if response.status_code == 200:
+        data = response.json()
+        # One member data being returned at a time.
+        if data["personlista"]["@hits"] == "1":
+            member = data["personlista"]["person"]
+            filtered_member = filter_member_dict(member)
+            return filtered_member
+        # In case a member detail is non-existent,
+        # returning an empty dict to indicate that 
+        # the detail does not exist in the member data.
+        elif data["personlista"]["@hits"] == "0":
+            return {}
+    else:
+        return {}
 
 
 def filter_member_dict(member):
@@ -51,92 +137,6 @@ def filter_member_dict(member):
         "intressent_id": member.get("intressent_id")
     }
     return membersDict
-
-
-def get_member_data(intressent_id):
-    """
-    Get a member data from the member's api
-    and filter for relevant keys.
-    """
-
-    domain = 'http://data.riksdagen.se'
-    members = 'personlista'
-    format_type = 'json'
-    response = requests.get('{}/{}/?iid={}&utformat={}'.format(domain, members, intressent_id, format_type))
-    if response.status_code == 200:
-        data = response.json()
-        # One member data being returned at a time.
-        if data["personlista"]["@hits"] == "1":
-            member = data["personlista"]["person"]
-            filtered_member = filter_member_dict(member)
-            return filtered_member, response.status_code
-        # In case a member detail is non-existent,
-        # returning an empty dict to indicate that 
-        # the detail does not exist in the member data.
-        elif data["personlista"]["@hits"] == "0":
-            return {}, response.status_code
-    else:
-        return {}, response.status_code
-
-
-def get_speeches_data(anftyp, size):
-    """
-    Get speeches data from speech's api
-    and filter for relevant keys.
-    For each speech we query the member api 
-    to get the member data and 
-    then merge it with the speech data.
-    """
-
-    domain = 'http://data.riksdagen.se'
-    speeches = 'anforandelista'
-    format_type = 'json'
-    response = requests.get('{}/{}/?anftyp={}&sz={}&utformat={}'.format(domain, speeches, anftyp, size, format_type))
-    if response.status_code == 200:
-        data = response.json()
-        # When a server responds with more than one speech.
-        if data["anforandelista"]["@antal"] > "1":
-            speeches = data["anforandelista"]["anforande"]
-            filtered_speeches = filter_speeches_dict(speeches)
-            for speech in filtered_speeches:
-                member_data, code = get_member_data(speech["intressent_id"])        
-                speech.update(member_data)
-            return filtered_speeches, code
-        # When a server responds only with one speech.
-        # A dict is returned and to handle this, the dict is stored 
-        # in a list because the filter_speeches_dict accepts a list.
-        elif data["anforandelista"]["@antal"] == "1":
-            speeches = [
-                data["anforandelista"]["anforande"]
-            ]
-            filtered_speeches = filter_speeches_dict(speeches)
-            for speech in filtered_speeches:
-                member_data, code = get_member_data(speech["intressent_id"])
-                speech.update(member_data)
-            return filtered_speeches, code
-    else:
-        return [], response.status_code
-        
-
-def create_app():
-    app = Flask("Parliament Challenge")
-
-    @app.route('/latest-speeches/', methods=['GET'])
-    def get_latest_speeches():
-        """
-        GET request:
-            curl -X GET "localhost:8080/latest-speeches/?anftyp=Nej&sz=10"
-        """
-        """
-        Clients can input the number of speeches required by them.
-        Result will be a merged data of speeches with members. 
-        """
-        anftyp = flask.request.args["anftyp"]
-        size = flask.request.args["sz"]
-        speeches, code = get_speeches_data(anftyp, size)
-        print(speeches)
-        return json.dumps(speeches), code
-    return app
 
 
 def main():
